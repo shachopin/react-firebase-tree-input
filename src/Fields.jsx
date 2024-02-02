65893e18261ff9addd0f26a744d462dd89eaa60e
Glitch (react-firebase-tree-input) <none>
2024-02-02 04:37:49 +0000

    💸🧸 Updated with Glitch

--- src/Fields.jsx
+++ src/Fields.jsx
@@ -1,12 +1,12 @@
 export const Fields = ({state, handleChange}) => (
   <>
-    <div className="col-xs-2">
+    <div className="col-xs-3">
       <input type="text" name="label" placeholder="Label" value={state.label} onChange={handleChange} className="form-control"></input>
     </div>
     <div className="col-xs-2">
       <input type="text" name="link" placeholder="Link" value={state.link} onChange={handleChange} className="form-control"></input>
     </div>
-    <div className="col-xs-5">
+    <div className="col-xs-6">
       <textarea name="desc" placeholder="Description" value={state.desc} onChange={handleChange} rows={1} className="form-control"></textarea>
     </div>
   </>
