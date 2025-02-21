2f44c033eadf590ad9feef60a6e7337d8e1daef0
Glitch (react-firebase-tree-input) <none>
2025-02-21 23:24:51 +0000

    🛳️🟫 Updated with Glitch

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,10 @@
 import update from "react-addons-update"; //https://facebook.github.io/react/docs/update.html
 
-const generateNewItemsObjectModifier = (oldItemsObjPointer, keysArray, callback) => {
+const generateNewItemsObjectModifier = (
+  oldItemsObjPointer,
+  keysArray,
+  callback
+) => {
   let modifierPointer = {};
   let modifier = modifierPointer;
   while (keysArray.length !== 0) {
@@ -19,8 +23,6 @@ const generateNewItemsObjectModifier = (oldItemsObjPointer, keysArray, callback)
   return modifier;
 };
 
-
-
 export const generateNewItemsObj = (oldItemsObj, indexArray, callback) => {
   return update(
     oldItemsObj,
@@ -29,14 +31,13 @@ export const generateNewItemsObj = (oldItemsObj, indexArray, callback) => {
 };
 
 export const pointToCurrentlySelectedNode = (oldItemsObjPointer, keysArray) => {
-
   while (keysArray.length !== 0) {
-    const removedKey= keysArray.shift();
+    const removedKey = keysArray.shift();
     oldItemsObjPointer = oldItemsObjPointer[removedKey].items;
   }
   return oldItemsObjPointer;
 };
 
-
 export const includesCaseInsensitive = (str, searchString) =>
-  new RegExp(searchString, 'i').test(str);
+  new RegExp(searchString, "i").test(str) ||
+  new RegExp(searchString, "i").test(str.replace(/-|\s+/g, ""));
